61fbf34d0ab503fce07ff7d9f620b5780eaf8a80
Justin Chen <jchen4086@gmail.com>
2024-04-13 14:47:03 -0400

    player walk animation

---
+++ PlayerAnimation.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var screen_center = get_viewport_rect().size / 2
+	if (mouse_pos.x < screen_center.x):
+		scale.x = abs(scale.x) * -1
+	else:
+		scale.x = abs(scale.x)
+	
+	if Input.is_action_pressed("MoveDown") or Input.is_action_pressed("MoveRight") or Input.is_action_pressed("MoveLeft") or Input.is_action_pressed("MoveUp"):
+		play("Walk")
+	else:
+		stop()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,14 +2,18 @@ extends CharacterBody2D
 
 
 var SPEED = 400
+var face_direction = Vector2()
+var player_sprite = null
 
-
-func _process(delta):
-	get_input()
+func _ready():
+	player_sprite = $AnimatedSprite2D
+	
+func _physics_process(delta):
+	get_input(face_direction)
 	set_velocity(velocity)
 	move_and_slide()
-	
-	
-func get_input():
-	var direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
+
+func get_input(direction):
+	direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
 	velocity = direction * SPEED
+	return direction != Vector2.ZERO
